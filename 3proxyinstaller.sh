758ef5d8524e24ef14af3adb0a556f64383eb7e8
JurgenZoller <76588165+JurgenZoller@users.noreply.github.com>
2021-03-16 11:06:17 +0300

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -10,8 +10,8 @@ mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
 wget --no-check-certificate https://github.com/JurgenZoller/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/JurgenZoller/3proxy/raw/master/.proxyauth
-chmod 600 /etc/3proxy/.proxyauth
+# wget --no-check-certificate https://github.com/JurgenZoller/3proxy/raw/master/.proxyauth
+# chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
 wget --no-check-certificate https://raw.github.com/JurgenZoller/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
